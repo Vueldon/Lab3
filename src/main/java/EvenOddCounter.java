f06d92b0e58b57db8f4c06fac0de4b1bafcfa866
vueld <vueld@192.168.1.7>
2019-09-30 00:13:59 -0500

    Completed

--- src/main/java/EvenOddCounter.java
+++ src/main/java/EvenOddCounter.java
@@ -1,3 +1,6 @@
+
+import java.util.Random;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,5 +13,37 @@
  */
 public class EvenOddCounter 
 {
-    
+
+    public static void main(String[] args) 
+    {
+        int evenTally = 0;
+        int oddTally = 0;
+        
+        for (int x = 0; x < 100; x++)
+        {
+            Random number = new Random();
+            int numberAsInteger = number.nextInt();
+            
+            if (IsEven(numberAsInteger) == true)
+            {
+                evenTally++;
+            } else
+            {
+                oddTally++;
+            }
+        }
+        
+        System.out.println("Number of even numbers: " + evenTally);
+        System.out.println("Number of odd numbers: " + oddTally);
+    }
+
+    public static boolean IsEven(int number) 
+    {
+        if ((number % 2) == 0) 
+        {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
